dd34de6c98ff54a2f308cb3172b8e5cd3cb14c0f
DHUsesAll <3099686@qq.com>
2016-05-09 16:30:10 +0800

    Initial Commit

---
+++ DHDrawerViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DHDrawerViewController
+//
+//  Created by DreamHack on 16/5/9.
+//  Copyright © 2016年 DreamHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
